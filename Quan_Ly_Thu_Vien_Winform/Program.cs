b2059f1e084ab77ce2af5ba9ae35c4079fa2a368
Tran Khanh <quockhanht03@outlook.com>
2024-12-09 08:52:49 +0700

    1

--- Quan_Ly_Thu_Vien_Winform/Program.cs
+++ Quan_Ly_Thu_Vien_Winform/Program.cs
@@ -18,38 +18,6 @@ namespace Quan_Ly_Thu_Vien_Winform
         [STAThread]
         static void Main()
         {
-            //string updateUrl = "https://github.com/Khanh779/Quan_Ly_Thu_Vien_Winform/raw/refs/heads/master/Quan_Ly_Thu_Vien_Winform/bin/Debug/Quan_Ly_Thu_Vien_Winform.exe";
-            //string fileName = updateUrl.Substring(updateUrl.LastIndexOf('/') + 1);
-            //string localPath = Application.StartupPath + "\\" + fileName;
-
-            //if (File.Exists(localPath))
-            //{
-            //    try
-            //    {
-            //        File.Delete(localPath);
-            //    }
-            //    catch (Exception ex)
-            //    {
-
-            //    }
-            //}
-
-            //using (WebClient client = new WebClient())
-            //{
-            //    try
-            //    {
-            //        client.DownloadFile(updateUrl, localPath);
-            //    }
-            //    catch (Exception ex)
-            //    {
-            //        MessageBox.Show("Lỗi khi tải file: " + ex.Message);
-            //        return;
-            //    }
-            //}
-
-
-
-            //MessageBox.Show("Đã cập nhật file " + fileName);
 
 
             Application.EnableVisualStyles();
